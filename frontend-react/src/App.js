53a8daddc22e10e252c5afa181b27e0701a458a5
Hoàng Phúc <134698287+hphuc193@users.noreply.github.com>
2024-12-21 03:26:16 +0000

    final

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'; // Sử dụng Routes thay cho Route
 import useSWR from 'swr';
+
+// Import các trang từ thư mục pages
+import Login from './pages/Login';
+import ListService from './pages/ListService';
+import ListRoom from './pages/ListRoom';
+import UserProfile from './pages/UserProfile';
+
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 function Room(props) {
@@ -15,25 +24,36 @@ function App() {
   if (isLoading) return <div>loading...</div>
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          {data.rooms.map((item) => <Room info={item} />)}
-        </ul>
-      </header>
-    </div>
+    <Router> {/* Bọc toàn bộ ứng dụng trong Router */}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Welcome to the Home Page</h3>
+
+          {/* Các liên kết điều hướng tới các trang */}
+          <nav>
+            <ul>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/listservice">List Service</Link></li>
+              <li><Link to="/listroom">List Room</Link></li>
+              <li><Link to="/userprofile">User Profile</Link></li>
+            </ul>
+          </nav>
+
+          <ul>
+            {data.rooms.map((item) => <Room key={item.room_id} info={item} />)}
+          </ul>
+        </header>
+
+        {/* Cấu hình các Route cho các trang */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/listservice" element={<ListService />} />
+          <Route path="/listroom" element={<ListRoom />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
